76fcef6b274f8489ce642c2d49c77ae2cd572a16
Andrea Giulianelli <andrygiulio@gmail.com>
2023-01-16 19:01:37 +0100

    ci: modify condition for documentation deploy

--- release.config.js
+++ release.config.js
@@ -1,10 +1,14 @@
 // Load this configuration that provide all the base for working with conventional commits
 const config = require('semantic-release-preconfigured-conventional-commits')
 
-// Commands executed during release
+/*
+ Commands executed during release.
+ It also set an environment variable "release" indicating that the release was successful.
+*/
 const publishCommands = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 2
 git push --force origin \${nextRelease.version} || exit 3
+echo "release_status=released" >> $GITHUB_ENV
 `
 // Only release on branch main
 const releaseBranches = ["main"]
@@ -23,15 +27,6 @@ config.plugins.push(
     ["@semantic-release/git", {
         "assets": ["CHANGELOG.md", "package.json"],
         "message": "chore(release)!: [skip ci] ${nextRelease.version} released"
-    }],
-    ["@qiwi/semrel-metabranch", {
-        "publish" : {
-            "action" : "push",
-            "from" : "build/dokka",
-            "to" : ".",
-            "branch" : "gh-pages",
-            "message" : "docs: ${nextRelease.version} documentation released"
-        }
     }]
 )
 
